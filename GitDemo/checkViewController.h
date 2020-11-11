96782f1f383b49f810829561ffc8f21a2b05e346
Abc <Abc>
2020-11-11 13:00:30 +0500

    first commit for ws

---
+++ GitDemo/checkViewController.h
@@ -0,0 +1,17 @@
+//
+//  checkViewController.h
+//  GitDemo
+//
+//  Created by SoloDev on 11/11/20.
+//  Copyright © 2020 SoloDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface checkViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitDemo/checkViewController.m
@@ -0,0 +1,32 @@
+//
+//  checkViewController.m
+//  GitDemo
+//
+//  Created by SoloDev on 11/11/20.
+//  Copyright © 2020 SoloDev. All rights reserved.
+//
+
+#import "checkViewController.h"
+
+@interface checkViewController ()
+
+@end
+
+@implementation checkViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
